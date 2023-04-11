7810c94deb4827377419aa4c7b3e40c67e54598b
tom <toms@live.jp>
2023-04-11 18:32:15 +0900

    test for testFunc

---
+++ __test__/index.test.ts
@@ -0,0 +1,8 @@
+import { beforeEach, afterEach, describe, it, expect, jest } from "@jest/globals";
+import { testFunc } from "@/index";
+
+describe("testFunc", () => {
+  it("returns a number what is 1 greater than the parameter value", () => {
+    expect(testFunc(1)).toBe(2);
+  });
+});
