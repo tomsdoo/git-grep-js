4452b18a6bf080a228ad4c519f8737afae983743
tom <toms@live.jp>
2023-04-12 18:48:41 +0900

    _matched is added

--- src/modules/StructuredLine.ts
+++ src/modules/StructuredLine.ts
@@ -1,9 +1,12 @@
 export class StructuredLine {
   protected _fileName: string;
+  protected _matched: boolean;
   protected _rawLine: string;
   constructor(rawLine: string) {
     this._rawLine = rawLine;
     this._fileName = rawLine.split("-")[0].split(":")[0];
+    const colonDividedFileName = rawLine.split(":")[0];
+    this._matched = colonDividedFileName === this._fileName;
   }
 
   public get fileName(): string {
