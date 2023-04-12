7f7d8fae0fbca24d40b020fdb6cc829fe03a59fc
tom <toms@live.jp>
2023-04-12 18:39:22 +0900

    _fileName is added

--- src/modules/StructuredLine.ts
+++ src/modules/StructuredLine.ts
@@ -1,7 +1,9 @@
 export class StructuredLine {
+  protected _fileName: string;
   protected _rawLine: string;
   constructor(rawLine: string) {
     this._rawLine = rawLine;
+    this._fileName = rawLine.split("-")[0].split(":")[0];
   }
 
   public get rawLine(): string {
