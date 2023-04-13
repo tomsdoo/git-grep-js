f3357c2f89a26ef19bd5f9cb0f8ee23a66a92341
tom <toms@live.jp>
2023-04-13 13:47:39 +0900

    test for SearchResultBlock

---
+++ __test__/modules/SearchResultBlock.test.ts
@@ -0,0 +1,33 @@
+import {
+  beforeEach,
+  afterEach,
+  describe,
+  it,
+  expect,
+  jest,
+} from "@jest/globals";
+import { SearchResultBlock } from "@/modules/SearchResultBlock";
+
+describe("SearchResultBlock", () => {
+  let rawLines: string[];
+  let blockedLinesStr: string;
+  beforeEach(() => {
+    rawLines = [
+      `src/sub/dir/1/file.ext-1-const a = 1;`,
+      `src/sub/dir/1/file.ext-2-`,
+      `src/sub/dir/1/file.ext:3:function testF(){`,
+      `src/sub/dir/1/file.ext-4-  return a;`,
+      `src/sub/dir/1/file.ext-5-}`,
+    ];
+    blockedLinesStr = rawLines.join("\n");
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("has _rawLines", () => {
+    const block = new SearchResultBlock(blockedLinesStr);
+    expect(block).toHaveProperty("_rawLines", rawLines);
+  });
+});
