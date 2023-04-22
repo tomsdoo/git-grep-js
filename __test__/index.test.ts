c0f15bb9125f6a11b570ee94a39c18023387e0af
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2023-04-22 10:47:54 +0900

    exporting classes (#23)
    * removal of console.log
    
    * exports classes
    
    * test for exports
    
    * testFunc() is removed
    
    * removal of test for testFunc()

--- __test__/index.test.ts
+++ __test__/index.test.ts
@@ -1,12 +1,20 @@
 import { afterEach, describe, it, expect, jest } from "@jest/globals";
-import { testFunc } from "@/index";
+import { SearchResultBlock, StructuredLine, Utility } from "@/index";
 
-describe("testFunc", () => {
+describe("exports", () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  it("returns a number what is 1 greater than the parameter value", () => {
-    expect(testFunc(1)).toBe(2);
+  it("SearchResultBlock is defined", () => {
+    expect(SearchResultBlock).not.toBeUndefined();
+  });
+
+  it("StructuredLine is defined", () => {
+    expect(StructuredLine).not.toBeUndefined();
+  });
+
+  it("Utility is defined", () => {
+    expect(Utility).not.toBeUndefined();
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-export function testFunc(a: number): number {
-  return a + 1;
-}
-
-console.log("test");
+export { SearchResultBlock } from "./modules/SearchResultBlock";
+export { StructuredLine } from "./modules/StructuredLine";
+export { Utility } from "./modules/Utility";
