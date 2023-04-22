f02203533c258f1931a2df78576c735799b7daf3
tom <toms@live.jp>
2023-04-22 10:42:01 +0900

    exports classes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+export { SearchResultBlock } from "./modules/SearchResultBlock";
+export { StructuredLine } from "./modules/StructuredLine";
+export { Utility } from "./modules/Utility";
+
 export function testFunc(a: number): number {
   return a + 1;
 }
